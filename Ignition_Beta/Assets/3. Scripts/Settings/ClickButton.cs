611fb5d0a0823218d5c28ff36718cf14ec82e321
oi1ioi1ioi1io <jeadan1010@gmail.com>
2024-07-04 15:08:41 +0900

    asdasd

---
+++ Ignition_Beta/Assets/3. Scripts/Settings/ClickButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickButton : MonoBehaviour
+{
+    [SerializeField]
+    Button general, audioBtn, graphic, control, gameplay, back;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
