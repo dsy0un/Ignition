ba77c64de1a406730c7132a4dab3a740105004b8
Schokolade796 <dmsdn796@gmail.com>
2024-07-05 15:23:01 +0900

    aa

--- Ignition_Beta/Assets/3. Scripts/FadeInOut/FadeInOut.cs
+++ Ignition_Beta/Assets/3. Scripts/FadeInOut/FadeInOut.cs
@@ -1,23 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
+
 
 public class FadeInOut : MonoBehaviour
 {
     public Image _image;
     public float _fadeTime = 1.5f;
+    [Tooltip ("FadeIn 사용시 0.1로 올라가는 순간에 키 값을 하나 추가해주세요. (키 값 총 3개)")]
     public AnimationCurve _fadeCurve;
 
+    [SerializeField] private UnityEvent onStartEvent;
+
+    private Camera mainCamera;
+    private bool fadeIn = false;
+    private bool fadeOut = false;
     private void Start()
     {
-        StartFadeIn();
+        mainCamera = GetComponentInParent<Camera>();
+        if (onStartEvent != null)
+        {
+            onStartEvent.Invoke();
+        }
     }
     public void StartFadeIn()
     {
         StartCoroutine(Fade(1, 0));
     }
-
+        
     public void StartFadeOut()
     {
         StartCoroutine(Fade(0, 1));
@@ -32,7 +45,6 @@ public class FadeInOut : MonoBehaviour
         {
             currentTime += Time.deltaTime;
             percent = currentTime / _fadeTime;
-
             Color color = _image.color;
             color.a = Mathf.Lerp(start, end, _fadeCurve.Evaluate(percent));
             _image.color = color;
