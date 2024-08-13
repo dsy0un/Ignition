1e27bdd4c3dbae61c70ad0b8d8f505477baa3c33
ADWI0 <jedminho@gmail.com>
2024-08-13 09:56:38 +0900

    d

--- Ignition_Beta/Assets/3. Scripts/Enemy/EnemyAnimation.cs
+++ Ignition_Beta/Assets/3. Scripts/Enemy/EnemyAnimation.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class EnemyAnimation : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(rb.velocity.magnitude);
     }
 }
