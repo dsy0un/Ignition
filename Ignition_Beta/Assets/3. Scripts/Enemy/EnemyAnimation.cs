85d59925e9e6554153be481b1f79aa4968670817
ADWI0 <jedminho@gmail.com>
2024-08-14 18:40:35 +0900

    d

--- Ignition_Beta/Assets/3. Scripts/Enemy/EnemyAnimation.cs
+++ Ignition_Beta/Assets/3. Scripts/Enemy/EnemyAnimation.cs
@@ -9,32 +9,40 @@ public class EnemyAnimation : MonoBehaviour
     Animator anim;
     private float xVel = 0.7f;
     private float zVel = 0.7f;
+    private bool isDead;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isDead)
         {
-            anim.SetTrigger("stinger");
+            anim.SetBool("isDead", true);
         }
-        print(rb.velocity);
-        if (rb.velocity.x >= xVel || rb.velocity.x <= -xVel || rb.velocity.z >= zVel || rb.velocity.z <= -zVel)
+        else
         {
-            anim.SetBool("isMove", true);
-            anim.SetFloat("xDir", rb.velocity.x);
-            anim.SetFloat("yDir", rb.velocity.z);
+            if (rb.velocity.x < xVel && rb.velocity.x > -xVel && rb.velocity.z < zVel && rb.velocity.z > -zVel)
+            {
+                anim.SetBool("isMove", false);
+                anim.SetFloat("xDir", 0);
+                anim.SetFloat("yDir", 0);
+            }
+            else
+            {
+                anim.SetBool("isMove", true);
+                anim.SetFloat("xDir", rb.velocity.x);
+                anim.SetFloat("yDir", rb.velocity.z);
+            }
         }
-        else if (rb.velocity.x < xVel && rb.velocity.x > -xVel && rb.velocity.z < zVel && rb.velocity.z > -zVel)
-        {
-            anim.SetBool("isMove", false);
-            anim.SetFloat("xDir", 0);
-            anim.SetFloat("yDir", 0);
-        }
+    }
+    public void isDie(bool die)
+    {
+        isDead = die;
     }
 }
---
+++ Ignition_Beta/Assets/3. Scripts/Enemy/EnemyState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState : MonoBehaviour, IHitAble
+{
+    EnemyAnimation enemyAnim;
+    private float maxHP = 100;
+    private float currentHP;
+    private float atk = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyAnim = GetComponent<EnemyAnimation>();
+        currentHP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Hit(float dmg)
+    {
+        currentHP -= dmg;
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        enemyAnim.isDie(true);
+        Destroy(gameObject);
+    }
+}
