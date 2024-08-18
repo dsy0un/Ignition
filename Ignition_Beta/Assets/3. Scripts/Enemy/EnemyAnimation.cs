511e5bfbb190719bd67047ec70c20f6d988e81e4
ADWI0 <jedminho@gmail.com>
2024-08-18 16:23:13 +0900

    d

--- Ignition_Beta/Assets/3. Scripts/Enemy/EnemyAnimation.cs
+++ Ignition_Beta/Assets/3. Scripts/Enemy/EnemyAnimation.cs
@@ -7,9 +7,10 @@ public class EnemyAnimation : MonoBehaviour
 {
     Rigidbody rb;
     Animator anim;
-    public float xVel = 0.7f;
-    public float zVel = 0.7f;
+    public float zVel = 0.7f; // 개체의 앞,뒤 속도 측정시 최소 속도
+    public float xVel = 0.7f; // 개체의 왼쪽,오른쪽 속도 측정시 최소 속도
     private bool isDead;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,23 +22,27 @@ public class EnemyAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 객체의 리지드바디 속도(글로벌 Vector3 형태)를 구한 후 로컬 형태로 변환
+        Vector3 velocity = transform.InverseTransformDirection(rb.velocity);
+
         if (isDead)
         {
             anim.SetBool("isDead", true);
         }
         else
         {
-            if (rb.velocity.x < xVel && rb.velocity.x > -xVel && rb.velocity.z < zVel && rb.velocity.z > -zVel)
+            // 속도 측정 값이 최솟값보다 작으면 애니메이션 멈춤
+            if (velocity.z > zVel || velocity.z < -zVel || velocity.x > xVel || velocity.x < -xVel)
+            {
+                anim.SetBool("isMove", true);
+                anim.SetFloat("xDir", velocity.z);
+                anim.SetFloat("yDir", velocity.x * 0.5f);
+            }
+            else
             {
                 anim.SetBool("isMove", false);
                 anim.SetFloat("xDir", 0);
                 anim.SetFloat("yDir", 0);
-            }
-            else
-            {
-                anim.SetBool("isMove", true);
-                anim.SetFloat("xDir", rb.velocity.x);
-                anim.SetFloat("yDir", rb.velocity.z);
             }
         }
     }
