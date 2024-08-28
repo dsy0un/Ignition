10a50926d918e97b3f1e5ca1f6daccc036f0da4f
oi1ioi1ioi1io <jeadan1010@gmail.com>
2024-08-28 16:50:10 +0900

    save

--- Ignition_Beta/Assets/3. Scripts/Item/TakeItem.cs
+++ Ignition_Beta/Assets/3. Scripts/Item/TakeItem.cs
@@ -1,22 +1,46 @@
+using EW;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 using Valve.VR.InteractionSystem;
 
 public class TakeItem : MonoBehaviour
 {
     [SerializeField]
     GameObject itemPrefab;
-    [SerializeField]
-    GameObject grabItem;
+
+    GameObject spawn;
+
+    bool isGrab = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            spawn = Instantiate(itemPrefab, other.transform.position, Quaternion.identity, other.transform);
+            spawn.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            spawn.GetComponent<Rigidbody>().isKinematic = false;
+            spawn.transform.SetParent(null);
+        }
+    }
 
     private void HandHoverUpdate(Hand hand)
     {
         GrabTypes grab = hand.GetGrabStarting();
-        bool isgrab = hand.IsGrabEnding(gameObject);
-        if (grab == GrabTypes.Grip && isgrab)
+        bool isgrab = hand.IsGrabEnding(spawn);
+        if (grab == GrabTypes.Grip && !isgrab)
         {
-            Instantiate(itemPrefab, hand.gameObject.transform.position, hand.gameObject.transform.rotation, hand.gameObject.transform);
+            spawn.GetComponent<Rigidbody>().isKinematic = false;
+            spawn.transform.SetParent(null);
         }
     }
 }
