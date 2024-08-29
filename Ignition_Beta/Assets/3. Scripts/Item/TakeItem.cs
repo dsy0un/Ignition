3a9dcc6caeb29819a10ef485a2c0d80f0012530a
oi1ioi1ioi1io <jeadan1010@gmail.com>
2024-08-29 14:34:21 +0900

    save
    TakeMag (전)완성본

--- Ignition_Beta/Assets/3. Scripts/Item/TakeItem.cs
+++ Ignition_Beta/Assets/3. Scripts/Item/TakeItem.cs
@@ -8,25 +8,50 @@ using Valve.VR.InteractionSystem;
 
 public class TakeItem : MonoBehaviour
 {
+    [Tooltip("0: Pistol, 1: Rifle, 2: Shotgun")]
     [SerializeField]
-    GameObject itemPrefab;
+    GameObject[] itemPrefab; // 0: Pistol, 1: Rifle, 2: Shotgun
+
+    [SerializeField]
+    Hand leftHand, rightHand;
+
+    GameObject currentObject;
 
     GameObject spawn;
-
-    bool isGrab = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Hand"))
         {
-            spawn = Instantiate(itemPrefab, other.transform.position, Quaternion.identity, other.transform);
-            spawn.GetComponent<Rigidbody>().isKinematic = true;
+            if (leftHand.currentAttachedObject == null && rightHand.currentAttachedObject == null) return;
+            if (leftHand.currentAttachedObject != null && rightHand.currentAttachedObject != null) return;
+
+            if (other.CompareTag("Hand"))
+            {
+                if (leftHand.currentAttachedObject != null)
+                    currentObject = leftHand.currentAttachedObject;
+                else if (rightHand.currentAttachedObject != null)
+                    currentObject = rightHand.currentAttachedObject;
+
+                switch (currentObject.tag)
+                {
+                    case "Pistol":
+                        break;
+                    case "Rifle":
+                        spawn = Instantiate(itemPrefab[1], other.transform.position, Quaternion.identity, other.transform);
+                        break;
+                    case "Shotgun":
+                        break;
+                    default: break;
+                }
+                spawn.GetComponent<Rigidbody>().isKinematic = true;
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Hand"))
+        if (currentObject != null && other.CompareTag("Hand"))
         {
             spawn.GetComponent<Rigidbody>().isKinematic = false;
             spawn.transform.SetParent(null);
