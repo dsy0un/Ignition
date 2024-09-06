0750287dc58540708627884b8130e0e2be360f35
ADWI0 <jedminho@gmail.com>
2024-09-06 15:28:30 +0900

    d

--- Ignition_Beta/Assets/3. Scripts/Weapon/Bolt.cs
+++ Ignition_Beta/Assets/3. Scripts/Weapon/Bolt.cs
@@ -19,6 +19,7 @@ public class Bolt : MonoBehaviour
     private Vector3 originPosition;
     private Quaternion originRotation;
     public float endPositionValue;
+    public float startPositionValue;
     public float impulsePower;
     public int jointValue;
     public bool redyToShot;
@@ -59,7 +60,7 @@ public class Bolt : MonoBehaviour
             joint.spring = jointValue; // 스프링 조인트 작동
             if (interactable.attachedToHand != null || !gun.isGrab) // 노리쇠를 잡고 있거나 총을 잡고 있지 않을 때
                 joint.spring = 0; // 스프링 조인트 끄기
-            if (transform.localPosition.z >= originPosition.z - 0.01f) // 노리쇠의 로컬 위치가 초기 위치 - 0.01 위치일 때
+            if (transform.localPosition.z >= originPosition.z - startPositionValue) // 노리쇠의 로컬 위치가 초기 위치 이상일 때
             {
                 transform.localPosition = originPosition; // 노리쇠 로컬 위치 = 초기 위치
                 if (boltRetraction)
