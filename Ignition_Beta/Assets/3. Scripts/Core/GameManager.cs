bd1939f181657190eec845db441ecaecd9c959a6
Schokolade796 <dmsdn796@gmail.com>
2024-09-06 11:35:31 +0900

    a

--- Ignition_Beta/Assets/3. Scripts/Core/GameManager.cs
+++ Ignition_Beta/Assets/3. Scripts/Core/GameManager.cs
@@ -59,30 +59,6 @@ public class GameManager : MonoBehaviour
     {
 
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     /// <summary>
     /// 디펜스 성공 이벤트
     /// </summary>
