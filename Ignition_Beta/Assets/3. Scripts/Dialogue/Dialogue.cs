cec5af594662686c739cf3ad482803548a83c68e
DSYOUN <jeadan1010@gmail.com>
2024-09-07 17:31:10 +0900

    save

--- Ignition_Beta/Assets/3. Scripts/Dialogue/Dialogue.cs
+++ Ignition_Beta/Assets/3. Scripts/Dialogue/Dialogue.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = int.MaxValue)]
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Text/Dialogue", order = int.MaxValue)]
 public class Dialogue : ScriptableObject
 {
-    [SerializeField]
+    [SerializeField, Tooltip("여기에 글 쓰면 됨")]
     string[] sentences;
     public string[] Sentences { get { return sentences; } }
 }
