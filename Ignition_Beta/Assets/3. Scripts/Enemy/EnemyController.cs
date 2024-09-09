7e91233fbfd26913cfce3f99fd05024d2f14b2e3
Schokolade796 <dmsdn796@gmail.com>
2024-09-09 20:00:27 +0900

    save

--- Ignition_Beta/Assets/3. Scripts/Enemy/EnemyController.cs
+++ Ignition_Beta/Assets/3. Scripts/Enemy/EnemyController.cs
@@ -1,17 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using static Unity.Burst.Intrinsics.X86;
 
 public class EnemyController : MonoBehaviour, IHitAble
 {
     [SerializeField]
     private float maxHP = 100;
     [SerializeField]
-    private float dmg = 10;
+    private float dmg = 10; 
+    public NavMeshSurface nms;
+
 
     private float currentHP;
+    private int volume; // 적이 쫒아 가는 우선순위
+    private Transform target; // 쫒아갈 목표
 
     private EnemyAnimation enemyAnim;
+    private NavMeshAgent nma;
+
+    private void Awake()
+    {
+        nma = GetComponent<NavMeshAgent>();
+    }
+    private void Start()
+    {
+        StartCoroutine(CoroutineUpdate());
+    }
+
+
+    IEnumerator CoroutineUpdate()
+    {
+        while (true)
+        {
+            yield return null;
+            Debug.Log(32);
+            nma.SetDestination(target.position);
+
+        }
+    }
+
+    public void ListenFollow(int sVolume, Transform sTarget)
+    {
+        target = sTarget;
+        volume = sVolume;
+    }
+    public void ListenReset()
+    {
+        nma.ResetPath();
+        volume = 0;
+    }
 
     public void Hit(float dmg, string coliName)
     {
