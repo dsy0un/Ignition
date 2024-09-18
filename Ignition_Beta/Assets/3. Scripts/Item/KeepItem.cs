ade2f45cd079c80d3f7a3bb31f0cba942404b1e2
dsy0un <jeadan1010@gmail.com>
2024-09-18 20:02:26 +0900

    save

--- Ignition_Beta/Assets/3. Scripts/Item/KeepItem.cs
+++ Ignition_Beta/Assets/3. Scripts/Item/KeepItem.cs
@@ -11,28 +11,35 @@ public class KeepItem : MonoBehaviour
     [SerializeField]
     GameObject containedObject;
 
-    [SerializeField]
     GameObject currentObject;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Pistol"))
         {
-
-        }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Hand"))
-        {
             if (leftHand.currentAttachedObject)
                 currentObject = leftHand.currentAttachedObject;
             else if (rightHand.currentAttachedObject)
                 currentObject = rightHand.currentAttachedObject;
 
+            currentObject.transform.SetParent(transform.parent, false);
+            currentObject.GetComponent<Rigidbody>().isKinematic = true;
+            currentObject.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.Euler(30, 0, 0));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pistol"))
+        {
+            if (!leftHand.currentAttachedObject || !rightHand.currentAttachedObject) return;
+            if (leftHand.currentAttachedObject)
+                currentObject = leftHand.currentAttachedObject;
+            else if (rightHand.currentAttachedObject)
+                currentObject = rightHand.currentAttachedObject;
+
+            currentObject.transform.SetParent(null);
             currentObject.GetComponent<Rigidbody>().isKinematic = false;
-            currentObject.transform.SetParent(null, false);
         }
     }
 }
