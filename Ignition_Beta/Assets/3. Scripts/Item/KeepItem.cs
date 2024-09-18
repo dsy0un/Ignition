5c2fa38b82776d40d98a2a97213c7ea4f7e513eb
dsy0un <jeadan1010@gmail.com>
2024-09-18 18:42:10 +0900

    save

--- Ignition_Beta/Assets/3. Scripts/Item/KeepItem.cs
+++ Ignition_Beta/Assets/3. Scripts/Item/KeepItem.cs
@@ -8,55 +8,32 @@ public class KeepItem : MonoBehaviour
 {
     [SerializeField]
     Hand leftHand, rightHand; // 플레이어 왼손, 오른손
+    [SerializeField]
+    GameObject containedObject;
 
-    GameObject currentObject; // 현재 들고 있는 오브젝트
+    [SerializeField]
+    GameObject currentObject;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Pistol"))
         {
-            if (leftHand.currentAttachedObject != null) // 왼손에 들고 있는 오브젝트가 있을 때
-                currentObject = leftHand.currentAttachedObject; // currentObject에 들고 있는 오브젝트 추가
-            else if (rightHand.currentAttachedObject != null) // 오른손에 들고 있는 오브젝트가 있을 때
-                currentObject = rightHand.currentAttachedObject; // currentObject에 들고 있는 오브젝트 추가
 
-            currentObject.transform.SetParent(transform.parent);
-            currentObject.transform.localPosition = Vector3.zero;
-            currentObject.GetComponent<Rigidbody>().isKinematic = true;
-            currentObject.GetComponent<Interactable>().attachedToHand.DetachObject(currentObject);
-            currentObject = null;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Pistol"))
+        if (other.CompareTag("Hand"))
         {
-            if (leftHand.currentAttachedObject == null && rightHand.currentAttachedObject == null) return; // 양손 다 들고 있지 않을 때
+            if (leftHand.currentAttachedObject)
+                currentObject = leftHand.currentAttachedObject;
+            else if (rightHand.currentAttachedObject)
+                currentObject = rightHand.currentAttachedObject;
 
-            if (leftHand.currentAttachedObject != null) // 왼손에 들고 있는 오브젝트가 있을 때
-                currentObject = leftHand.currentAttachedObject; // currentObject에 들고 있는 오브젝트 추가
-            else if (rightHand.currentAttachedObject != null) // 오른손에 들고 있는 오브젝트가 있을 때
-                currentObject = rightHand.currentAttachedObject; // currentObject에 들고 있는 오브젝트 추가
-            
-            currentObject.transform.SetParent(null);
             currentObject.GetComponent<Rigidbody>().isKinematic = false;
+            currentObject.transform.SetParent(null, false);
         }
     }
-
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.CompareTag("Pistol"))
-    //    {
-    //        if (leftHand.currentAttachedObject == null && rightHand.currentAttachedObject == null) return; // 양손 다 들고 있지 않을 때
-
-    //        if (leftHand.currentAttachedObject != null) // 왼손에 들고 있는 오브젝트가 있을 때
-    //            currentObject = leftHand.currentAttachedObject; // currentObject에 들고 있는 오브젝트 추가
-    //        else if (rightHand.currentAttachedObject != null) // 오른손에 들고 있는 오브젝트가 있을 때
-    //            currentObject = rightHand.currentAttachedObject; // currentObject에 들고 있는 오브젝트 추가
-
-    //        currentObject.transform.SetParent(null);
-    //        currentObject.GetComponent<Rigidbody>().isKinematic = false;
-    //    }
-    //}
 }
+
