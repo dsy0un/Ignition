23ee534a59a3fe13e6d23a85de5fb5071bfb00ac
dsy0un <jeadan1010@gmail.com>
2024-09-19 09:54:58 +0900

    save

--- Ignition_Beta/Assets/3. Scripts/Item/KeepItem.cs
+++ Ignition_Beta/Assets/3. Scripts/Item/KeepItem.cs
@@ -17,29 +17,50 @@ public class KeepItem : MonoBehaviour
     {
         if (other.CompareTag("Pistol"))
         {
-            if (leftHand.currentAttachedObject)
-                currentObject = leftHand.currentAttachedObject;
-            else if (rightHand.currentAttachedObject)
-                currentObject = rightHand.currentAttachedObject;
+            if (leftHand.currentAttachedObject != null) // 왼손에 들고 있는 오브젝트가 있을 때
+            {
+                currentObject = leftHand.currentAttachedObject; // currentObject에 들고 있는 오브젝트 추가
+                currentObject.transform.SetParent(transform);
+                currentObject.GetComponent<Rigidbody>().isKinematic = true;
+                currentObject.transform.SetLocalPositionAndRotation(transform.position, Quaternion.Euler(new Vector3(30, 0, 0)));
+            }
+            else if (rightHand.currentAttachedObject != null) // 오른손에 들고 있는 오브젝트가 있을 때
+            {
+                currentObject = rightHand.currentAttachedObject; // currentObject에 들고 있는 오브젝트 추가
+                currentObject.transform.SetParent(currentObject.transform);
+                currentObject.GetComponent<Rigidbody>().isKinematic = true;
+                currentObject.transform.SetLocalPositionAndRotation(transform.position, Quaternion.Euler(new Vector3(30, 0, 0)));
+            }
+        }
+    }
 
-            currentObject.transform.SetParent(transform.parent, false);
-            currentObject.GetComponent<Rigidbody>().isKinematic = true;
-            currentObject.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.Euler(30, 0, 0));
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Pistol") && transform.childCount > 0)
+        {
+            Transform child = transform.GetChild(0);
+            child.SetParent(transform);
+            child.GetComponent<Rigidbody>().isKinematic = true;
+            child.SetLocalPositionAndRotation(transform.position, Quaternion.Euler(new Vector3(30, 0, 0)));
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Pistol"))
+        if (other.CompareTag("Hand") && transform.childCount > 0)
         {
-            if (!leftHand.currentAttachedObject || !rightHand.currentAttachedObject) return;
-            if (leftHand.currentAttachedObject)
-                currentObject = leftHand.currentAttachedObject;
-            else if (rightHand.currentAttachedObject)
-                currentObject = rightHand.currentAttachedObject;
-
-            currentObject.transform.SetParent(null);
-            currentObject.GetComponent<Rigidbody>().isKinematic = false;
+            if (leftHand.currentAttachedObject != null) // 왼손에 들고 있는 오브젝트가 있을 때
+            {
+                currentObject = leftHand.currentAttachedObject; // currentObject에 들고 있는 오브젝트 추가
+                currentObject.transform.SetParent(null);
+                currentObject.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            else if (rightHand.currentAttachedObject != null) // 오른손에 들고 있는 오브젝트가 있을 때
+            {
+                currentObject = rightHand.currentAttachedObject; // currentObject에 들고 있는 오브젝트 추가
+                currentObject.transform.SetParent(null);
+                currentObject.GetComponent<Rigidbody>().isKinematic = false;
+            }
         }
     }
 }
--- Ignition_Beta/Assets/SteamVR/InteractionSystem/Core/Scripts/BodyCollider.cs
+++ Ignition_Beta/Assets/SteamVR/InteractionSystem/Core/Scripts/BodyCollider.cs
@@ -29,19 +29,23 @@ namespace Valve.VR.InteractionSystem
         //-------------------------------------------------
         void FixedUpdate()
 		{
-            Quaternion targetRotation = head.localRotation;
-            if (targetRotation.eulerAngles.x <= 45f)
-            {
-                float distanceFromFloor = Vector3.Dot(head.localPosition, Vector3.up);
-                //cc.height = Mathf.Max(cc.radius, distanceFromFloor);
-                //cc.height = Mathf.Clamp(cc.height, 0, cc.height);
-                transform.localPosition = head.localPosition - distanceFromFloor * Vector3.up;
-                // transform.localPosition = new Vector3(transform.position.x, 1.5f, transform.position.z);
+            KeepItemFollowHead();
+        }
 
-                Quaternion bodyY = Quaternion.Euler(0, targetRotation.eulerAngles.y, 0);
-                // Quaternion bodyY = Quaternion.Euler(0, targetRotation.eulerAngles.y, 0);
-                transform.localRotation = Quaternion.Lerp(transform.localRotation, bodyY, 3 * Time.deltaTime);
-            }
+        void KeepItemFollowHead()
+        {
+            Quaternion targetRotation = head.localRotation;
+            if (targetRotation.eulerAngles.x >= 45f && targetRotation.eulerAngles.x <= 330f) return;
+
+            float distanceFromFloor = Vector3.Dot(head.localPosition, Vector3.up);
+            //cc.height = Mathf.Max(cc.radius, distanceFromFloor);
+            //cc.height = Mathf.Clamp(cc.height, 0, cc.height);
+            transform.localPosition = head.localPosition - distanceFromFloor * Vector3.up;
+            // transform.localPosition = new Vector3(transform.position.x, 1.5f, transform.position.z);
+
+            Quaternion bodyY = Quaternion.Euler(0, targetRotation.eulerAngles.y, 0);
+            // Quaternion bodyY = Quaternion.Euler(0, targetRotation.eulerAngles.y, 0);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, bodyY, 3 * Time.deltaTime);
         }
 	}
 }
