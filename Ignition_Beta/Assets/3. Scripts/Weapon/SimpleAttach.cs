bd7a784d9318246aa5ee71d05a99ffc201c8f6d6
ADWI0 <jedminho@gmail.com>
2024-09-20 17:30:28 +0900

    d

---
+++ Ignition_Beta/Assets/3. Scripts/Weapon/SimpleAttach.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SimpleAttach : MonoBehaviour
+{
+    private Interactable interactable;
+    public GameObject originPos;
+
+    private void Awake()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    private void Update()
+    {
+        transform.position = originPos.transform.position;
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes grabTypes = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        if (interactable.attachedToHand == null && grabTypes != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabTypes);
+            hand.HoverLock(interactable);
+        }
+        else if (isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(interactable);
+        }
+    }
+}
