2493241d86338218a3c1cd53369ce83f3582cb5f
dsy0un <jeadan1010@gmail.com>
2024-09-21 14:50:16 +0900

    save

--- Ignition_Beta/Assets/3. Scripts/LookOut/Terminal.cs
+++ Ignition_Beta/Assets/3. Scripts/LookOut/Terminal.cs
@@ -1,23 +1,49 @@
 using Michsky.UI.Shift;
+using System.Collections;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
 
 public class Terminal : MonoBehaviour
 {
-    ModalWindowManager window;
+    [SerializeField]
+    bool sharpAnimations = false;
+
     Interactable openWindow;
     Animator runMonitor;
+    Animator terminal;
+    bool isOn = false;
 
     private void Awake()
     {
         runMonitor = transform.parent.GetComponent<Animator>();
-        window = GetComponentInChildren<ModalWindowManager>();
+        terminal = transform.GetChild(0).GetComponent<Animator>();
     }
 
-    void HandHoverUpdate(Hand hand)
+    void OnHandHoverBegin(Hand hand)
     {
-        runMonitor.SetBool("IsOn", true);
-        if (runMonitor.GetBool("IsOn") && runMonitor.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
-            window.ModalWindowIn();
+        if (runMonitor.GetBool("IsOn"))
+        {
+            ModalAnim("Terminal Out");
+            if (terminal.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+                runMonitor.SetBool("IsOn", false);
+        }
+        else
+        {
+            runMonitor.SetBool("IsOn", true);
+            if (runMonitor.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+                ModalAnim("Terminal In");
+        }
+    }
+
+    /// <summary>
+    /// 터미널 관련 애니메이션 실행 함수
+    /// </summary>
+    /// <param name="animName"></param>
+    public void ModalAnim(string animName)
+    {
+        if (sharpAnimations == false)
+            terminal.CrossFade(animName, 0.1f);
+        else
+            terminal.Play(animName);
     }
 }
