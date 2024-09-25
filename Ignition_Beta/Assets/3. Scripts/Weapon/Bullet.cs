9c92e827371934005abe35bdb951a160ddc46677
Schokolade796 <dmsdn796@gmail.com>
2024-09-25 15:05:46 +0900

    save

--- Ignition_Beta/Assets/3. Scripts/Weapon/Bullet.cs
+++ Ignition_Beta/Assets/3. Scripts/Weapon/Bullet.cs
@@ -7,29 +7,37 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private float destructionDelay = 30f;
     [SerializeField] private float damage;
+    public GameObject hit; //삭제 필
     private Gun gun;
     private float time;
+    private Rigidbody rb; // 삭제 필요
+    public int shotSpeed = 100; // 삭제 필요
 
     bool isUpgrade = false;
 
     private void Awake()
     {
         gun = GetComponentInParent<Gun>();
+        rb = GetComponentInParent<Rigidbody>();
     }
 
     private void Update()
     {
-        time += Time.deltaTime;
-        if (time >= destructionDelay)
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            gun.ReturnObject(gameObject);
-            time = 0;
+            rb.AddRelativeForce(Vector3.fwd * shotSpeed, ForceMode.Impulse);
         }
-        if (!isUpgrade && GameManager.Instance.isBulletUpgrade)
-        {
-            isUpgrade = true;
-            damage *= 1.5f;
-        }
+        //time += Time.deltaTime;
+        //if (time >= destructionDelay)
+        //{
+        //    gun.ReturnObject(gameObject);
+        //    time = 0;
+        //}
+        //if (!isUpgrade && GameManager.Instance.isBulletUpgrade)
+        //{
+        //    isUpgrade = true;
+        //    damage *= 1.5f;
+        //}
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -40,9 +48,10 @@ public class Bullet : MonoBehaviour
             if (collision.transform.root.TryGetComponent<IHitAble>(out var h))
             {
                 h.Hit(damage, coliName);
+                Instantiate(hit, transform.position , Quaternion.Euler(Vector3.back));
                 Destroy(this.gameObject);
             }
         }
-        else gun.ReturnObject(gameObject);
+        //else gun.ReturnObject(gameObject);
     }
 }
